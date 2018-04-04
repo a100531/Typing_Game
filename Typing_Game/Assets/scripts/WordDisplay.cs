7f37e5556051bab3a37a6e464b71b9af7c0982b0
Daniel <daniel.cassar.a100531@mcast.edu.mt>
2018-04-04 22:30:02 +0200

    fixed folder hierarchy

---
+++ Typing_Game/Assets/scripts/WordDisplay.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class WordDisplay : MonoBehaviour {
+
+	public Text text;
+	public float fallSpeed = 1f;
+
+	public GameObject wordDestroyEffect;
+
+	public void SetWord (string word)
+	{
+		text.text = word;
+	}
+	public void RemoveLetter()
+	{
+		text.text = text.text.Remove(0,1);
+		text.color = Color.red;
+	}
+	public void RemoveWord()
+	{
+		Instantiate(wordDestroyEffect,transform.position,transform.rotation);
+		Destroy(gameObject);
+	}
+
+		void Update()
+	{
+		transform.Translate(0f,-fallSpeed * Time.deltaTime,0f);
+	}
+	void OnTriggerEnter2D(Collider2D other)
+	{
+		if (other.tag == "GameOver")
+		{
+			Debug.Log("Hit");
+			Time.timeScale = 0;
+		}
+		
+	}
+
+}
